6720517a547816106167d0e55606a823739360b5
Glitch (nebulous-ripple-eggnog) <none>
2021-03-25 05:08:52 +0000

    👷🎐 Updated with Glitch

---
+++ script.js
@@ -0,0 +1,151 @@
+var mistakeCount;
+var pattern = [4, 5, 2, 2, 1, 3];
+const patternLength = 10;
+var progress = 0;
+var gamePlaying = false;
+var tonePlaying = false;
+var volume = 0.5;
+var guessCounter = 0;
+var clueHoldTime = 1000;
+const cluePauseTime = 333; //how long to pause in between clues
+const nextClueWaitTime = 1000; //how long to wait before starting playback of the clue sequence
+
+function startGame() {
+  mistakeCount = 0
+  console.log(getRandomPattern());
+  progress = 0;
+  gamePlaying = true;
+  document.getElementById("startBtn").classList.add("hidden");
+  document.getElementById("stopBtn").classList.remove("hidden");
+  playClueSequence();
+  
+}
+
+function getRandomPattern() {
+  pattern = [];
+  for (var i=0, t=10; i<t; i++) {
+    pattern.push(Math.round(Math.random() * 4)+1)
+}
+  return pattern;
+}
+
+function stopGame() {
+  gamePlaying = false;
+  document.getElementById("startBtn").classList.remove("hidden");
+  document.getElementById("stopBtn").classList.add("hidden");
+}
+
+function lightButton(btn) {
+  document.getElementById("button"+btn).classList.add("lit");
+  showImg(btn);
+}
+function clearButton(btn) {
+  document.getElementById("button"+btn).classList.remove("lit");
+  hideImg(btn);
+}
+
+function showImg(num) {
+  document.getElementById("img"+num).classList.remove("hidden");
+}
+function hideImg(num) { 
+  document.getElementById("img"+num).classList.add("hidden");
+}
+
+function playSingleClue(btn){
+  if(gamePlaying){
+    lightButton(btn);
+    playTone(btn,clueHoldTime);
+    setTimeout(clearButton,clueHoldTime,btn);
+  }
+}
+
+function playClueSequence(){
+  guessCounter = 0;
+  let delay = nextClueWaitTime; //set delay to initial wait time
+  for(let i=0;i<=progress;i++){ // for each clue that is revealed so far
+    console.log("play single clue: " + pattern[i] + " in " + delay + "ms")
+    setTimeout(playSingleClue,delay,pattern[i]) // set a timeout to play that clue
+    clueHoldTime -= 10;
+    delay += clueHoldTime 
+    delay += cluePauseTime;
+  }
+}
+
+function loseGame(){
+  stopGame();
+  alert("Game Over. You lost.");
+}
+
+function winGame(){
+  stopGame();
+  alert("Game Over. You won!");
+}
+
+function guess(btn){
+  console.log("user guessed: " + btn);
+  if(!gamePlaying){
+    return;
+  }
+
+  if(pattern[guessCounter] == btn){
+    if(guessCounter == progress){
+      if(progress == pattern.length - 1){
+        winGame();
+        document.getElementById("count").textContent = "Mistake Count: 0";
+      }else{
+        progress++;
+        playClueSequence();
+      }
+    }else{
+      guessCounter++;
+    }
+  }else{
+    mistakeCount++;
+    document.getElementById("count").textContent = "Mistake Count: " + mistakeCount;
+    console.log("mistake count: " + mistakeCount);
+    if(mistakeCount == 3){
+      loseGame();
+      document.getElementById("count").textContent = "Mistake Count: 0";
+    } else {
+      playClueSequence();
+    }
+      
+  }
+  
+} 
+
+// Sound Synthesis Functions
+const freqMap = {
+  1: 230,
+  2: 300,
+  3: 200,
+  4: 450,
+  5: 180
+}
+function playTone(btn,len){ 
+  o.frequency.value = freqMap[btn]
+  g.gain.setTargetAtTime(volume,context.currentTime + 0.05,0.025)
+  tonePlaying = true
+  setTimeout(function(){
+    stopTone()
+  },len)
+}
+function startTone(btn){
+  if(!tonePlaying){
+    o.frequency.value = freqMap[btn]
+    g.gain.setTargetAtTime(volume,context.currentTime + 0.05,0.025)
+    tonePlaying = true
+  }
+}
+function stopTone(){
+    g.gain.setTargetAtTime(0,context.currentTime + 0.05,0.025)
+    tonePlaying = false
+}
+
+var context = new AudioContext()
+var o = context.createOscillator()
+var g = context.createGain()
+g.connect(context.destination)
+g.gain.setValueAtTime(0,context.currentTime)
+o.connect(g)
+o.start(0)
